8c79d993cbf7b6f2324f48792a76fd74097fa797
Tanisha Fonseca <71168034+tanishaf28@users.noreply.github.com>
2026-04-11 15:56:33 -0400

    Leetcode 168: Return columntile for given integer

---
+++ excetile.py
@@ -0,0 +1,42 @@
+"""Excel Column Tile
+168. Excel Sheet Column T
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+"""
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        result = ""
+        
+        while columnNumber > 0:
+            columnNumber -= 1
+            remainder = columnNumber % 26
+            result += chr(65 + remainder)
+            columnNumber //= 26
+        
+        return result[::-1]
