dbb16165fbff0f3831d51def8b97fd2eb0009f30
Tanisha Fonseca <71168034+tanishaf28@users.noreply.github.com>
2026-04-23 11:15:48 -0400

    Leetcode 26: remove duplicates from sorted array

---
+++ Duplicate.cpp
@@ -0,0 +1,21 @@
+"""
+Leetcode 26: remove duplicates from sorted array 
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+"""
+  
+if (nums.size() == 0) return 0;
+
+        int i = 0;
+
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; 
+    }
+};
